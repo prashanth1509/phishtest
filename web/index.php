c3294d99aa5595e66f0c33395003db2c11519a72
prashanth.a <prashanth.a@flipkart.com>
2015-04-27 14:13:48 +0530

    index

--- web/index.php
+++ web/index.php
@@ -13,8 +13,20 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello';
+$index_page = <<<l
+<!Doctype html>
+<head>
+<title>Flipkart</title>
+</head>
+<body style="font-family:Arial;text-align:center">
+    <h2>Win something</h2>
+    <hr>
+    <h3>1. Visit this page on your phone</h3>
+    <h3>2. Visit below link</h3>
+    <a style="background:#027CD5;padding:10px 20px;text-decoration:none;color:#fff" href="http://www.google.com">Register</a>
+</body>
+l;
+return $index_page;
 });
 
 $app->run();
